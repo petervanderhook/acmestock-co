d31a4748766d50622b06c963381a7a0b36901fe0
petervanderhook <petervanderhook@gmail.com>
2023-11-08 09:45:57 -0800

    Added host=0.0.0.0

--- audit/app.py
+++ audit/app.py
@@ -62,4 +62,4 @@ app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
-    app.run(port=8110, use_reloader=False)
+    app.run(host='0.0.0.0', port=8110, use_reloader=False)
--- processing/app.py
+++ processing/app.py
@@ -89,4 +89,4 @@ app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
     init_scheduler()
-    app.run(port=8100, use_reloader=False)
+    app.run(host='0.0.0.0', port=8100, use_reloader=False)
